5bf0704999d206475bcd43635f4fadea5eb0ced6
Sean Keever <swkeever@gmail.com>
2019-08-28 09:27:49 -0700

    consolidates location page

--- src/pages/location.js
+++ src/pages/location.js
@@ -31,10 +31,13 @@ const Location = (props) => {
     <Layout location="/location/">
       <Head title="Location" />
       <Hero id="driving-directions">
-        <div className="columns">
+        <div className="columns is-vcentered">
           <div className="column">
-            <h2 className="is-size-2">Driving Directions</h2>
-            <p className="is-size-4">Where are you coming from?</p>
+            <div className="content">
+              <h2>Driving Directions</h2>
+              <p>Where are you coming from?</p>
+            </div>
+
             <div className="m-t-md buttons">
               <button
                 type="button"
@@ -51,31 +54,17 @@ const Location = (props) => {
                   South
               </button>
             </div>
-          </div>
-          <div className="column">
-            <div className="container">
-              <div className="content">
-                <p>
-                  {`Coming from the ${north ? 'North' : 'South'}:`}
-                </p>
-                <ol>
-                  {directions}
-                </ol>
-              </div>
-              <div className="container has-text-right">
-                <a href="/location/#map" className="button is-primary">See Map</a>
-              </div>
+            <div className="content">
+              <p>
+                {`Directions coming from ${north ? 'north' : 'south'}:`}
+              </p>
+              <ol>
+                {directions}
+              </ol>
             </div>
           </div>
-        </div>
-      </Hero>
-      <Hero id="map">
-        <div className="columns is-one-third">
-          <div className="column">
-            <h2 className="is-size-2">Where We're Located</h2>
-            <p className="is-size-4">Zoom in on the map to take a closer look.</p>
-          </div>
           <div className="column is-two-thirds">
+            <p className="help is-info m-b-sm">Zoom in on the map to take a closer look.</p>
             <Map />
           </div>
         </div>
