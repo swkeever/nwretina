a774cb4f00ece8420e67f9cd2aeb89cfa787303c
Sean Keever <swkeever@gmail.com>
2019-09-07 14:59:48 -0700

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,39 +2,15 @@ import React from 'react';
 import {
   Layout,
   Head,
-  Image,
-  Hero,
-  ContactButton,
-  Header,
+  Content,
 } from '../components';
 import slugs from '../utils/slugs';
-import getContent from '../functions/get-content';
 
-const About = () => {
-  //const about1 = getContent(slugs.about.one);
-
-  return null;
-
-  return (
-    <Layout location="/about/">
-      <Head title="About" />
-      <Hero id="about">
-        <div className="columns is-vcentered">
-          <Header content={about1.header} />
-          <div className="column">
-            <div
-              className="content"
-              dangerouslySetInnerHTML={{ __html: about1.html }}
-            />
-          </div>
-          <div className="column">
-            <Image src={about1.image.src} alt={about1.image.alt} />
-          </div>
-        </div>
-        <ContactButton />
-      </Hero>
-    </Layout>
-  );
-};
+const About = () => (
+  <Layout location="/about/">
+    <Head title="About" />
+    <Content slugPrefix={slugs.about} />
+  </Layout>
+);
 
 export default About;
