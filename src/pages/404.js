6398160ed3236c40ad29a1156bb02bcbc2b15a9d
Sean Keever <swkeever@gmail.com>
2019-09-08 13:28:40 -0700

    404 page hotfix

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,17 +6,17 @@ import {
   Layout,
   Head,
 } from '../components';
-import navLinks from '../utils/routes';
+import navLinks, { notFound } from '../utils/routes';
 
 const NotFound = () => (
   <Layout>
-    <Head location={navLinks.notFound.href} />
+    <Head location={notFound.href} />
     <Hero
       id="not-found"
       color="primary"
       image="/uploads/pnw2.jpg"
     >
-      <Header>{navLinks.notFound.name}</Header>
+      <Header>{notFound.name}</Header>
       <div className="buttons">
         <Link to={navLinks.home.href} className="button is-white is-outlined">{navLinks.home.name}</Link>
         <Link to={navLinks.contact.href} className="button is-white is-outlined"> {navLinks.contact.name}</Link>
