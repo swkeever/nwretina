2ecb451a4a37d505be15fb15c388196570c57a70
Sean Keever <swkeever@gmail.com>
2019-09-21 12:25:20 -0700

    tries better csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,16 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const csp = "default-src 'none'; connect-src 'self'; font-src data:; frame-src https://www.google.com; img-src 'self' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://identity.netlify.com/v1/netlify-identity-widget.js https://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/api2/v1566858990656/recaptcha__en.js; style-src 'unsafe-inline'";
+const csp = `
+default-src 'none';
+connect-src 'self';
+font-src data:;
+frame-src https://www.google.com;
+img-src 'self' data:;
+script-src 'strict-dynamic' 'nonce-LgD4Zk/ALDBCCFvVCUS/xA==' 'unsafe-inline' https://identity.netlify.com/v1/netlify-identity-widget.js https://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/api2/v1566858990656/recaptcha__en.js;
+style-src 'unsafe-inline';
+base-uri 'self'
+`;
 
 module.exports = {
   siteMetadata: {
