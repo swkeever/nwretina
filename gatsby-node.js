f52e1e172331d642477cc2f0629c1195b2e07b3e
Sean Keever <swkeever@gmail.com>
2019-10-03 20:06:17 -0700

    adds redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,3 +11,33 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     });
   }
 };
+
+
+exports.createPages = ({ actions }) => {
+  const { createRedirect } = actions;
+  createRedirect({
+    fromPath: '/everett',
+    toPath: '/',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/medical-records',
+    toPath: '/',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/everett/kirkland',
+    toPath: '/',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/hello-world',
+    toPath: '/',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/billing-insurance/kirkland',
+    toPath: '/',
+    isPermanent: true,
+  });
+};
